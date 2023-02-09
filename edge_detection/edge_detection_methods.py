ca18df856fe0892715908509db8d1e23bbeea575
joshvin144 <joshvin144@gmail.com>
2023-02-09 12:56:03 -0800

    Added Laplacian filter; added filter for 2D kernel

--- edge_detection/edge_detection_methods.py
+++ edge_detection/edge_detection_methods.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 # Debugging
 from icecream import ic
 
-# The Sobel Kernel
-
 # We may look at the rate of change through the definition of derivative:
 # [f(x + h) - f(x)]/[(x + h) - x] = [f(x + h) - f(x)]/h
 # In vector form, this looks like [-1, 0, 1] multiplied by [xf(x - h), f(x), f(x + h)]
@@ -26,11 +24,23 @@ from icecream import ic
 # A row vector is a derivative with respect to x
 # A column vector is a derivative with respect to y
 
+# It is important to note that the image indices start at (0,0) in the upper righthand corner and increase to the right and down
+# We orient the filters to match this convention
+
+# The Sobel Kernel
+# The Sobel Kernel is the product of the derivative vector and the Gauss vector
 DERIVATIVE_X = np.array([[-1, 0, 1]], dtype = np.float32)
 DERIVATIVE_Y = np.array([[-1], [0], [1]], dtype = np.float32)
 
 GAUSS_X = np.array([[1, 2, 1]], dtype = np.float32)
 GAUSS_Y = np.array([[1], [2], [1]], dtype = np.float32)
+
+# The Laplacian Kernel
+# The Laplacian Kernel is the equivalent of the second derivative
+# This will pinpoint the center of the edge
+LAPLACIAN_XY = np.array([[0, -1, 0],
+	                     [-1, 4, -1],
+	                     [0, -1, 0]], dtype = np.float32)
 
 def filter_x(kernel, img):
 	""" kernel: A 2D vector, where the size of the second dimension is 1
@@ -69,5 +79,28 @@ def filter_y(kernel, img):
 			# Multiply the elements of the kernel by the elements of the image
 			for idx in range(kernel_size_y):
 				new_img[row][col] += (kernel[idx][0]*img[row + idx][col])
+
+	return new_img
+
+def filter_xy(kernel, img):
+	""" kernel: A 2D array
+	    img: A 2D image that is already zero padded """
+	
+	kernel_size_x = kernel.shape[1] # Number of columns
+	kernel_size_y = kernel.shape[0] # Number of rows
+	img_size_x = img.shape[1] # Number of columns
+	img_size_y = img.shape[0] # Number of rows
+	new_img_size_x = img_size_x - (kernel_size_x - 1) # Number of columns
+	new_img_size_y = img_size_y - (kernel_size_y - 1) # Number of rows
+
+	new_img = np.zeros((new_img_size_y, new_img_size_x))
+	# Start in the upper lefthand corner of the img: (0, 0)
+	# Move the kernel
+	for row in range(new_img_size_y):
+		for col in range(new_img_size_x):
+			# Multiply the elements of the kernel by the elements of the image
+			for row_idx in range(kernel_size_y):
+				for col_idx in range(kernel_size_x):
+					new_img[row][col] += (kernel[row_idx][col_idx]*img[row + row_idx][col + col_idx])
 
 	return new_img
--- edge_detection/main.py
+++ edge_detection/main.py
@@ -7,8 +7,10 @@ from edge_detection_methods import DERIVATIVE_X
 from edge_detection_methods import DERIVATIVE_Y
 from edge_detection_methods import GAUSS_X
 from edge_detection_methods import GAUSS_Y
+from edge_detection_methods import LAPLACIAN_XY
 from edge_detection_methods import filter_x
 from edge_detection_methods import filter_y
+from edge_detection_methods import filter_xy
 
 # Debug
 from icecream import ic
@@ -20,6 +22,7 @@ B = 2
 
 # Path to image
 path = "./480px-SheppLogan_Phantom.svg.png"
+
 
 # Add command line arguments, here
 def create_argument_parser():
@@ -52,9 +55,12 @@ def main():
 	edge_y = filter_x(DERIVATIVE_Y, gauss_y)
 	gauss_x = filter_y(GAUSS_X, edge_y)
 
+	# Edge detection using the second derivative
+	laplace_xy = filter_xy(LAPLACIAN_XY, img_xy)
+
 	if (args.plot):
-		plt.imshow(edge_y)
 		plt.imshow(gauss_x)
+		# plt.imshow(laplace_xy)
 		plt.show()
 
 	return 0
